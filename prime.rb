e4519868c141889591eb6e19b45b59a587f30dff
Tanya Bihag <tbihag@gmail.com>
2019-08-25 03:48:57 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(num)
+   #it's easier to return false condition first
+   #cuz prime numbers are always great than 1
+   return false if num < 2
+   #create a range start from 2 until nth num
+   range = 2..num-1 #num-1: if num doesn't divide anything less than it => true
+   #each: iterate through each index of the array
+   range.each do |x|
+   #prime numbers can only divide themselves and 1
+      return false if num % x == 0
+   end
+   true
+end
